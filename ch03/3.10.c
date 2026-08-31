2f7b268e6b3a8ceefde9f0df24b27304633b8ab3
AleksandarBiberdzic99 <ydwmg6dtby@privaterelay.appleid.com>
2026-08-31 16:56:18 +0200

    Update 3.10.c

--- ch03/3.10.c
+++ ch03/3.10.c
@@ -5,6 +5,27 @@
 //  Created by Aleksandar on 20. 7. 2026..
 //
 
+/*******************************************************************
+ *  Zadatak 3.10 — Umetanje broja u ureden niz                     *
+ *                                                                 *
+ *  Program umece novi broj u vec ureden niz tako da niz ostane    *
+ *  ureden. Obraduje se proizvoljan broj kompleta ulaznih          *
+ *  podataka.                                                      *
+ *                                                                 *
+ *  Krecuci od kraja niza, elementi se pomeraju za jedno mesto     *
+ *  prema kraju sve dok su veci od novog broja. Broj se zatim      *
+ *  upisuje iza prvog nepomerenog elementa, odnosno na pocetak     *
+ *  niza ako su svi elementi bili pomereni.                        *
+ *                                                                 *
+ *  n? 12                                                          *
+ *  A? 2 4 6 8 10 12 14 16 18 20 22 24                             *
+ *  b? 9                                                           *
+ *  A= 2 4 6 8 9 10 12 14 16 18 20 22 24                           *
+ *                                                                 *
+ *  Niz je dimenzionisan na N+1 da bi bilo mesta za novi element.  *
+ *******************************************************************/
+
+
 #include <stdlib.h>
 #include <stdio.h>
 
